825537c8089a7c194ba5f531c438f86dcb0a02f6
gokhul <gokhul.tg@galvanizeme.com>
2024-08-10 01:55:57 +0530

    typing-game logic implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lorem } from 'faker';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'typing-game';
+  sentence = lorem.sentence();
+  enteredText = '';
+
+  getSentence() {
+    return this.sentence;
+  }
+
+  onInputChange(event: Event) {
+    const eventValue = event.target as HTMLInputElement;
+    this.enteredText = eventValue.value;
+  }
+
+  isGameOver() {
+    console.log(
+      'called is game over----------------',
+      this.sentence === this.enteredText
+    );
+    return this.sentence === this.enteredText;
+  }
 }
